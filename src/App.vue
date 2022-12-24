a5828c199288d4d71cebde00e4603a09f08051ff
fehooaaa <dmarenkov2008@gmail.com>
2022-12-24 15:00:29 +0300

    Messages are loaded from API and showed in chat window

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Container>
+      <ChatWindow>
+        <ChatMessage v-for="message in messages" :key="message" v-bind:username="message.author" v-bind:datetime="message.datetime" v-bind:text="message.text"></ChatMessage>
+      </ChatWindow>
+    </Container>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Container from "./components/MyContainer.vue";
+import ChatMessage from "./components/ChatMessage.vue";
+import ChatWindow from "./components/ChatWindow.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
-</script>
+    Container, ChatMessage, ChatWindow
+  },
 
+  data() {
+    return{
+      messages: []
+    }
+  },
+
+  methods: {
+    GetMessages(){
+      this.axios.get('https://61bcd385d8542f0017824a2a.mockapi.io/messages')
+        .then((response) => {
+          for(let i in response.data){
+            this.messages.push(response.data[i]);
+          }
+        });
+    }
+  },
+
+  mounted(){
+    this.GetMessages();
+  }
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  background-color: #f9f9fa;
 }
 </style>
